25496a62ac6ffe352bd63ded0bffa9a8fdb7cbb8
jerry george <jerrygithub360@gmail.com>
2024-12-31 01:51:40 +0100

    registered blueprints

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from flasgger import Swagger
 from models import *
 from auth.auth import auth
 from oauth.api import oauth
+from download.download import download
+from episode.episode import episode
+from favourite.favourite import favourite
+from playlist.playlist import playlist
+from playlistitem.playlistitem import playlistitem
+from podcast.podcast import podcast
+from rating.rating import rating
+from sharedplaylist.sharedplaylist import shared_playlist
+from subscription.subscription import subscription
+from users.users import users
+
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -14,11 +25,24 @@ swagger = Swagger(app)
 
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=3)
+
 db.init_app(app)
 migrate = Migrate(app,db)
 app.register_blueprint(auth)
 app.register_blueprint(oauth)
+app.register_blueprint(download)
+app.register_blueprint(episode)
+app.register_blueprint(favourite)
+app.register_blueprint(playlist)
+app.register_blueprint(playlistitem)
+app.register_blueprint(podcast)
+app.register_blueprint(rating)
+app.register_blueprint(shared_playlist)
+app.register_blueprint(subscription)
+app.register_blueprint(users)
+
 
 @app.errorhandler(401)
 def unauthorized(error):
